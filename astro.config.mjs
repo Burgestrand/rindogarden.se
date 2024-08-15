2fe03104b7bd0342621d7d48569339794caf60bb
Kim Burgestrand <kim@burgestrand.se>
2024-08-15 12:34:45 +0200

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.rindögården.se',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false /* Disable base styles, we use custom tailwind.css */,
-    })
-  ]
+  integrations: [tailwind({
+    applyBaseStyles: false /* Disable base styles, we use custom tailwind.css */
+  }), sitemap()]
 });
