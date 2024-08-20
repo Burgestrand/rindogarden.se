69dee6936c39ba8ee978b291c097c380d8e18e73
Kim Burgestrand <kim@burgestrand.se>
2024-08-20 22:10:54 +0200

    Add default "tema ej spikat" to events

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,7 +3,7 @@ import { z, defineCollection } from 'astro:content'
 const events = defineCollection({
   type: 'content',
   schema: z.object({
-    title: z.string().optional(),
+    title: z.string().optional().default("Tema ej spikat"),
     group: z.enum(['åk 4-6']).default('åk 4-6'),
     time: z.string().time().default('18:00:00'),
     duration: z.string().duration().default('PT3H'),
