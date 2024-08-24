8f0b676e293edf52dc5f2129221adba66599dbcb
Kim Burgestrand <kim@burgestrand.se>
2024-08-24 13:26:34 +0200

    Title and adults are the two pieces of data we *really* need

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,10 +4,10 @@ const events = defineCollection({
   type: 'content',
   schema: z.object({
     title: z.string().optional().default("Tema ej spikat"),
+    adults: z.array(z.string()).default([]),
     group: z.enum(['åk 4-6']).default('åk 4-6'),
     time: z.string().time().default('18:00:00'),
     duration: z.string().duration().default('PT3H'),
-    adults: z.array(z.string()).default([]),
   }),
 })
 
