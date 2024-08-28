1ca9aa1ccfea3603429d4efb9b64bb3d3cf3563e
Kim Burgestrand <kim@burgestrand.se>
2024-08-28 13:15:23 +0200

    Add grillpremiär summary

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const events = defineCollection({
   schema: z.object({
     title: z.string().optional().default("Tema ej spikat"),
     adults: z.array(z.string()).default([]),
+    summary: z.string().optional().default(""),
     group: z.enum(['åk 4-6']).default('åk 4-6'),
     time: z.string().time().default('18:00:00'),
     duration: z.string().duration().default('PT3H'),
