1cd8f2d7550fea4c13d20cab6ecb452f8d1605d1
Kim Burgestrand <kim@burgestrand.se>
2024-09-05 17:57:27 +0200

    Update all FAQ/engage content

--- src/common.ts
+++ src/common.ts
@@ -3,3 +3,12 @@
  */
 
 export const consentFormURL = "https://gnasweden.typeform.com/to/ap59Mwsl"
+
+export const workgroup = [
+  { name: "Kim Burgestrand", email: "kim@burgestrand.se" },
+  { name: "Anna-Lena M.W." },
+  { name: "Sandra B." },
+]
+
+export const email = "rindogarden@gmail.com"
+export const facebookURL = "https://www.facebook.com/rindogarden"
