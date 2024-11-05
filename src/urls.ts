0329532e3997fca41d7d6809f6bc7ce4f9a0e235
Kim Burgestrand <kim@burgestrand.se>
2024-11-05 16:12:54 +0100

    Add medlemsregler to website FAQ

--- src/urls.ts
+++ src/urls.ts
@@ -1,25 +1,39 @@
-import { rindobornaSwishNumber } from "@/common"
+import { rindobornaSwishNumber } from "@/common";
 
 // Swish URLs below.
 
-function rindobornaSwishURL({ amount, message, edit }: { amount?: string, message: string, edit?: string }) {
+function rindobornaSwishURL({
+  amount,
+  message,
+  edit,
+}: {
+  amount?: string;
+  message: string;
+  edit?: string;
+}) {
   const swishSearchParams = new URLSearchParams({
     sw: rindobornaSwishNumber.replace(/ /g, ""),
     cur: "SEK",
     msg: message,
     edit: edit ?? "amt,msg",
     src: "qr",
-  })
+  });
 
   if (amount) swishSearchParams.set("amt", amount);
 
-  return "https://app.swish.nu/1/p/sw/?" + swishSearchParams.toString().replace(/\+/g, "%20")
+  return (
+    "https://app.swish.nu/1/p/sw/?" +
+    swishSearchParams.toString().replace(/\+/g, "%20")
+  );
 }
 
 export const swish = {
-  member: rindobornaSwishURL({ amount: "200", message: "Årsmedlemsskap Rindöborna: Hushållets namn & mail" }),
-  kiosk: rindobornaSwishURL({ message: "Rindögården Kiosk", edit: "amt" })
-}
+  member: rindobornaSwishURL({
+    amount: "200",
+    message: "Årsmedlemsskap Rindöborna: Hushållets namn & mail",
+  }),
+  kiosk: rindobornaSwishURL({ message: "Rindögården Kiosk", edit: "amt" }),
+};
 
 export default {
   home: "/",
@@ -29,6 +43,7 @@ export default {
   engage: "/engagera-dig",
   belastningsregistret: "/engagera-dig/belastningsregistret",
   checklist: "/engagera-dig/checklista",
+  medlemsregler: "/medlemsregler.pdf",
 
   faq: "/faq",
 
@@ -38,8 +53,9 @@ export default {
     consentForm: "https://gnasweden.typeform.com/to/ap59Mwsl",
 
     polisen: {
-      utdrag: "https://polisen.se/tjanster-tillstand/belastningsregistret/barn-annan-verksamhet/",
-      kontroll: "https://etjanster.polisen.se/eregisterutdrag/kontroll"
-    }
-  }
-}
+      utdrag:
+        "https://polisen.se/tjanster-tillstand/belastningsregistret/barn-annan-verksamhet/",
+      kontroll: "https://etjanster.polisen.se/eregisterutdrag/kontroll",
+    },
+  },
+};
