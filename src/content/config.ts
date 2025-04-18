bba28f38fe769803aef1be0a28cebfc40839602f
Kim Burgestrand <kim@burgestrand.se>
2025-04-18 11:09:00 +0200

    Disco has enough adults with Sandra this time

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,6 +5,7 @@ const events = defineCollection({
   schema: z.object({
     title: z.string().optional().default("Rindögården"),
     adults: z.array(z.string()).default([]),
+    requiredAdults: z.number().min(1).default(2),
     summary: z.string().optional().default(""),
     group: z.enum(["åk 4-6"]).default("åk 4-6"),
     time: z.string().time().default("18:30:00"),
--- src/events.ts
+++ src/events.ts
@@ -1,76 +1,93 @@
-import { getCollection } from 'astro:content'
-import type { CollectionEntry } from 'astro:content'
-import { Temporal } from "@js-temporal/polyfill"
+import { getCollection } from "astro:content";
+import type { CollectionEntry } from "astro:content";
+import { Temporal } from "@js-temporal/polyfill";
 
-const locale = 'sv-SE'
-const timeZone = 'Europe/Stockholm'
+const locale = "sv-SE";
+const timeZone = "Europe/Stockholm";
 
-type OriginalEvent = CollectionEntry<'events'>
+type OriginalEvent = CollectionEntry<"events">;
 export type Event = OriginalEvent & {
-  id: string,
-  date: Temporal.PlainDate,
-  time: Temporal.PlainTime,
-  duration: Temporal.Duration,
-  missingAdults: number,
-  url: string
-}
+  id: string;
+  date: Temporal.PlainDate;
+  time: Temporal.PlainTime;
+  duration: Temporal.Duration;
+  missingAdults: number;
+  url: string;
+};
 
 async function augment(event: OriginalEvent): Promise<Event> {
-  const date = Temporal.PlainDate.from(event.slug)
-  const time = Temporal.PlainTime.from(event.data.time)
-  const duration = Temporal.Duration.from(event.data.duration)
-  const missingAdults = Math.max(2 - event.data.adults.length, 0)
-  const url = `/events/${event.slug}`
-  const copy = Object.create(event)
-  const id = `event:${date.toString()}`
-  return Object.assign(copy, { id, date, time, duration, url, missingAdults }) as Event
+  const date = Temporal.PlainDate.from(event.slug);
+  const time = Temporal.PlainTime.from(event.data.time);
+  const duration = Temporal.Duration.from(event.data.duration);
+  const requiredAdults = event.data.requiredAdults;
+  const missingAdults = Math.max(requiredAdults - event.data.adults.length, 0);
+  const url = `/events/${event.slug}`;
+  const copy = Object.create(event);
+  const id = `event:${date.toString()}`;
+  return Object.assign(copy, {
+    id,
+    date,
+    time,
+    duration,
+    url,
+    missingAdults,
+  }) as Event;
 }
 
 function compare(a: Event, b: Event) {
-  return Temporal.PlainDate.compare(a.date, b.date)
+  return Temporal.PlainDate.compare(a.date, b.date);
 }
 
 export async function list() {
-  const collection = await getCollection('events')
-  const events = await Promise.all(collection.map(augment))
-  events.sort(compare)
-  return events
+  const collection = await getCollection("events");
+  const events = await Promise.all(collection.map(augment));
+  events.sort(compare);
+  return events;
 }
 
 export const timestamp = {
-  iso: (event: Event) => event.date.toZonedDateTime({ timeZone, plainTime: event.time }).toString({ timeZoneName: 'never' }),
-  full: (event: Event) => `${date.full(event)}, kl. ${time.full(event)}`
-}
+  iso: (event: Event) =>
+    event.date
+      .toZonedDateTime({ timeZone, plainTime: event.time })
+      .toString({ timeZoneName: "never" }),
+  full: (event: Event) => `${date.full(event)}, kl. ${time.full(event)}`,
+};
 
-type Custom = (object: { date: Temporal.PlainDate, locale: string, timeZone: string }) => any;
+type Custom = (object: {
+  date: Temporal.PlainDate;
+  locale: string;
+  timeZone: string;
+}) => any;
 
-export const isPast = (event: Event) => Temporal.PlainDate.compare(
-  event.date,
-  Temporal.Now.plainDateISO()
-) < 0
+export const isPast = (event: Event) =>
+  Temporal.PlainDate.compare(event.date, Temporal.Now.plainDateISO()) < 0;
 
 export const date = {
-  full: (event: Event) => event.date.toLocaleString(locale, { dateStyle: "full" }),
-  short: (event: Event) => event.date.toLocaleString(locale, { dateStyle: "short" }),
-  shortMonth: (event: Event) => event.date.toLocaleString(locale, { month: "short" }).replace(/\.$/, ''),
-  custom: (event: Event, fn: Custom) => fn({ date: event.date, locale, timeZone })
-}
+  full: (event: Event) =>
+    event.date.toLocaleString(locale, { dateStyle: "full" }),
+  short: (event: Event) =>
+    event.date.toLocaleString(locale, { dateStyle: "short" }),
+  shortMonth: (event: Event) =>
+    event.date.toLocaleString(locale, { month: "short" }).replace(/\.$/, ""),
+  custom: (event: Event, fn: Custom) =>
+    fn({ date: event.date, locale, timeZone }),
+};
 
 export const time = {
   full: (event: Event) => {
-    const from = event.time
-    const to = from.add(event.duration)
-    return `${from.toLocaleString(locale, { timeStyle: "short" })} - ${to.toLocaleString(locale, { timeStyle: "short" })}`
+    const from = event.time;
+    const to = from.add(event.duration);
+    return `${from.toLocaleString(locale, { timeStyle: "short" })} - ${to.toLocaleString(locale, { timeStyle: "short" })}`;
   },
   tiny: (event: Event) => {
-    const from = event.time
-    const to = from.add(event.duration)
-    const ljust = (n: number) => n.toString().padStart(2, '0')
+    const from = event.time;
+    const to = from.add(event.duration);
+    const ljust = (n: number) => n.toString().padStart(2, "0");
 
     if (from.minute === 0 && to.minute === 0) {
-      return `${from.hour}-${to.hour}`
+      return `${from.hour}-${to.hour}`;
     } else {
-      return `${from.hour}:${ljust(from.minute)}-${to.hour}:${ljust(to.minute)}`
+      return `${from.hour}:${ljust(from.minute)}-${to.hour}:${ljust(to.minute)}`;
     }
-  }
-}
+  },
+};
